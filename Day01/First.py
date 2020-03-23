fd12efdc6e79031101fe4bbb14efcd8c3f04b052
BORBER <borber.drum@gmail.com>
2020-03-23 23:04:04 +0800

    第二个界面

--- Day01/First.py
+++ Day01/First.py
@@ -1,17 +1,15 @@
 import sys
-from PyQt5.QtWidgets import QApplication,QWidget
-
+from Day01.designer.MainWindow_ui import Ui_MainWindow
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
 
 if __name__ == '__main__':
     # 创建 Qapplication类的实例
     app = QApplication(sys.argv)
     # 创建一个窗口
-    w = QWidget()
-    # 设置尺寸
-    w.resize(300, 150)
-    # 窗口位置
-    w.setWindowTitle("第一个Pyqt窗口")
-    # 显示窗口
-    w.show()
+    mainWindow = QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(mainWindow)
+    mainWindow.setWindowTitle("第二个窗体")
+    mainWindow.show()
     # 进入程序的主循环 并通过 exit函数保证主循环安全结束
     sys.exit(app.exec_())
